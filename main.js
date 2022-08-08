384601d75a01cce6871f744b04968a535f94841f
Axel RHELIMI <axel.rhelimi@gmail.com>
2022-08-08 21:13:21 +0200

    update variable TOKEN commandsID and ClientID

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var Robot = require('./robot.js')
 var commandsList = require("./commands.json");
 
 
-const TOKEN = "MTAwMjMyODkyODk2ODUyNzkwMg.GR2KYk.ozTfOHNum2N0jO2yNvJj9gFgJ7K27374Jr2l3w";//process.env.TOKEN;
-const clientID = "1002328928968527902";//process.env.CLIENT_ID;
+const TOKEN = process.env.TOKEN;
+const clientID = process.env.CLIENT_ID;
 const commandsID = 
 {
-    general_channel_id:"1002330996756201555",
-    product_channel_id:"1004733860807966751"
+    general_channel_id: process.env.GENERAL_CHANNEL_ID,
+    product_channel_id: process.env.PRODUCT_CHANNEL_ID
 }
 var robot = new Robot(commandsID,commandsList,TOKEN,clientID);
 robot.__init__(()=>
